4c69c4fe867ad44754d0c88fde2b90cd04a315a7
AJ Clemente <areljann.c@gmail.com>
2025-04-09 02:05:45 -0400

    Use reusable components

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,12 +1,36 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
 	css: ['~/assets/css/main.css'],
 	vite: {
-		plugins: [
-			tailwindcss(),
-		],
+		plugins: [tailwindcss()]
 	},
+	components: [
+		{
+			path: '~/components/atoms',
+			pathPrefix: false,
+			extensions: ['.vue'],
+			watch: true
+		},
+		{
+			path: '~/components/molecules',
+			pathPrefix: false,
+			extensions: ['.vue'],
+			watch: true
+		},
+		{
+			path: '~/components/organisms',
+			pathPrefix: false,
+			extensions: ['.vue'],
+			watch: true
+		},
+		{
+			path: '~/components/layouts',
+			pathPrefix: false,
+			extensions: ['.vue'],
+			watch: true
+		}
+	]
 });
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,9 +5,9 @@ const goToBoard = () => {
 </script>
 
 <template>
-	<div class="w-screen h-screen flex flex-col justify-center items-center">
+	<FullScreen centered>
 		<h1 class="text-lg font-bold" id="title">gitlo</h1>
 		<button @click="goToBoard" class="p-2 rounded-md bg-blue-600 hover:bg-blue-700 text-white cursor-pointer">View
 			Boards</button>
-	</div>
+	</FullScreen>
 </template>
