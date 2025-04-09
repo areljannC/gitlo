bf6743348a53a0c45aa3493c899b4a465018cbff
AJ Clemente <areljann.c@gmail.com>
2025-04-09 01:03:53 -0400

    Create a home page

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const goToBoard = () => {
+	navigateTo('/boards');
+};
+</script>
+
+<template>
+	<div class="w-screen h-screen flex flex-col justify-center items-center">
+		<h1 class="text-lg font-bold" id="title">gitlo</h1>
+		<button @click="goToBoard" class="p-2 rounded-md bg-blue-600 hover:bg-blue-700 text-white cursor-pointer">View
+			Boards</button>
+	</div>
+</template>
