a8ca81dca8d5edde4d25f2a09feaf2340c99e6a4
AJ Clemente <areljann.c@gmail.com>
2025-04-09 02:05:29 -0400

    Create a reusable full screen component

---
+++ app/components/layouts/FullScreen/FullScreen.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps({
+	centered: {
+		type: Boolean,
+		default: false
+	}
+})
+
+const baseStyle = 'w-screen h-screen flex flex-col space-y-2'
+</script>
+
+<template>
+	<div :class="[baseStyle, centered && 'justify-center items-center']">
+		<slot />
+	</div>
+</template>
