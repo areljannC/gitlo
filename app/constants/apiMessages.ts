7c45ba1cffbdfceac7b699b38cdacc2a4dd41c87
AJ Clemente <areljann.c@gmail.com>
2025-04-15 14:00:47 -0400

    Create API message constants

---
+++ app/constants/apiMessages.ts
@@ -0,0 +1,11 @@
+// 500s
+export const SERVER_ERROR = 'Server error.';
+
+// 400s
+export const INVALID_REQUEST_ERROR = 'Invalid request.';
+export const BOARD_ID_REQUIRED_ERROR = 'Board ID is required.'
+export const NO_BOARDS_FOUND_ERROR = 'No boards found. '
+export const NO_BOARD_FOUND_ERROR = 'No board found with specified ID.'
+
+// 200s
+export const CREATE_BOARD_SUCCESS = 'Board created successfully!';
