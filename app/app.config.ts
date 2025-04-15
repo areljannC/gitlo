abea70e5fc39be4b9c6c1849dc788ab5cb94e38a
AJ Clemente <areljann.c@gmail.com>
2025-04-15 01:10:03 -0400

    Add custom styling to modal header

--- app/app.config.ts
+++ app/app.config.ts
@@ -6,6 +6,7 @@ export default defineAppConfig({
 	ui: {
 		modal: {
 			slots: {
+				header: 'flex justify-center items-center gap-1.5 p-4 sm:px-6 min-h-16',
 				body: 'flex-1 overflow-y-auto p-4 sm:p-6',
 				footer: 'flex items-center gap-2 p-4 sm:px-6 justify-end'
 			}
