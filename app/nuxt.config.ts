0bd8a97fef98a9506f13ddb7ae6f618e49b948ce
AJ Clemente <areljann.c@gmail.com>
2025-04-15 00:55:56 -0400

    Setup unit tests and coverage report

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,11 +1,21 @@
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
+	telemetry: false,
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
 	css: ['~/assets/css/main.css'],
 	vite: {
 		plugins: [tailwindcss()]
 	},
-	modules: ['@nuxt/ui', '@nuxt/icon', '@nuxt/test-utils/module']
+	components: [
+		{
+			path: '~/components',
+			pathPrefix: false
+		}
+	],
+	modules: ['@nuxt/ui', '@nuxt/icon', '@nuxt/test-utils/module'],
+	nitro: {
+		ignore: ['**/*.test.ts']
+	}
 });
--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -2,6 +2,23 @@ import { defineVitestConfig } from '@nuxt/test-utils/config';
 
 export default defineVitestConfig({
 	test: {
-		environment: 'nuxt'
+		environment: 'nuxt',
+		coverage: {
+			enabled: true,
+			provider: 'v8',
+			reporter: ['text', 'html', 'json'],
+			reportsDirectory: './coverage',
+			thresholds: {
+				lines: 100,
+				functions: 100,
+				branches: 100,
+				statements: 100
+			},
+			all: true,
+			clean: true,
+			cleanOnRerun: true,
+			include: ['components/**/*.{vue,ts}', 'utils/**/*.{vue,ts}', 'server/**/*.{vue,ts}'],
+			exclude: ['node_modules/', '.nuxt/', 'dist/', '**/*.d.ts', '**/*.test.ts', '**/*.spec.ts', '**/index.ts', '**/dev.vue']
+		}
 	}
 });
