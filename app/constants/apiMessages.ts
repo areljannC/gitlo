85aa8049fc2f2b499ebfbda087f7275915d9e475
AJ Clemente <areljann.c@gmail.com>
2025-04-15 23:22:39 -0400

    Add API message constant

--- app/constants/apiMessages.ts
+++ app/constants/apiMessages.ts
@@ -4,7 +4,7 @@ export const SERVER_ERROR = 'Server error.';
 // 400s
 export const INVALID_REQUEST_ERROR = 'Invalid request.';
 export const BOARD_ID_REQUIRED_ERROR = 'Board ID is required.'
-export const NO_BOARDS_FOUND_ERROR = 'No boards found. '
+export const NO_BOARDS_FOUND_ERROR = 'No boards found.'
 export const NO_BOARD_FOUND_ERROR = 'No board found with specified ID.'
 
 // 200s
