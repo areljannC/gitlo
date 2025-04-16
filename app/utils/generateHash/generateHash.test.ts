f9aaefa70d315873e10d58bd4f367a02ed5ff5d7
AJ Clemente <areljann.c@gmail.com>
2025-04-16 04:46:05 -0400

    Add an option to generate hash for unsynced IDs

--- app/utils/generateHash/generateHash.test.ts
+++ app/utils/generateHash/generateHash.test.ts
@@ -6,9 +6,49 @@ describe('generateHash', () => {
 		const hash = generateHash();
 		expect(hash).toBeTypeOf('string');
 	});
+
 	it('generates a hash with 8 characters', () => {
 		const hash = generateHash();
 		expect(hash).toHaveLength(8);
-		expect(hash).toBeTypeOf('string');
+	});
+
+	it('generates an unsynced hash with `__` prefix', () => {
+		const unsyncedHash = generateHash(true);
+		expect(unsyncedHash.startsWith('__')).toBe(true); // Check if it starts with `__`
+		expect(unsyncedHash).toHaveLength(10); // 8 characters + 2 for the prefix
+	});
+
+	it('generates different hashes on multiple calls', () => {
+		let hash1 = generateHash();
+		let hash2 = generateHash();
+		expect(hash1).not.toBe(hash2);
+
+		let unsyncedHash1 = generateHash(true);
+		let unsyncedHash2 = generateHash(true);
+		expect(unsyncedHash1).not.toBe(unsyncedHash2);
+
+		hash1 = generateHash();
+		hash2 = generateHash();
+		expect(hash1).not.toBe(hash2);
+
+		unsyncedHash1 = generateHash(true);
+		unsyncedHash2 = generateHash(true);
+		expect(unsyncedHash1).not.toBe(unsyncedHash2);
+
+		hash1 = generateHash();
+		hash2 = generateHash();
+		expect(hash1).not.toBe(hash2);
+
+		unsyncedHash1 = generateHash(true);
+		unsyncedHash2 = generateHash(true);
+		expect(unsyncedHash1).not.toBe(unsyncedHash2);
+
+		hash1 = generateHash();
+		hash2 = generateHash();
+		expect(hash1).not.toBe(hash2);
+
+		unsyncedHash1 = generateHash(true);
+		unsyncedHash2 = generateHash(true);
+		expect(unsyncedHash1).not.toBe(unsyncedHash2);
 	});
 });
--- app/utils/generateHash/generateHash.ts
+++ app/utils/generateHash/generateHash.ts
@@ -1,2 +1,5 @@
-// TODO: use `crypto` for a a more stable hash generation
-export const generateHash = (): string => Math.random().toString(36).substring(2, 10);
+// TODO: use `crypto` for a more stable hash generation
+export const generateHash = (isUnsynced = false): string => {
+	const hash = Math.random().toString(36).substring(2, 10);
+	return isUnsynced ? `__${hash}` : hash;
+};
