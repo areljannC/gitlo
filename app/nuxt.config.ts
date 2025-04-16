9e8e90a1bb1a5a0cc22bcd72d9f47867eebd3dd5
AJ Clemente <areljann.c@gmail.com>
2025-04-16 04:46:35 -0400

    Setup Pinia for state management

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -3,19 +3,10 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineNuxtConfig({
 	telemetry: false,
 	compatibilityDate: '2024-11-01',
-	devtools: { enabled: true },
+	devtools: { enabled: false },
 	css: ['~/assets/css/main.css'],
-	vite: {
-		plugins: [tailwindcss()]
-	},
-	components: [
-		{
-			path: '~/components',
-			pathPrefix: false
-		}
-	],
-	modules: ['@nuxt/ui', '@nuxt/icon', '@nuxt/test-utils/module'],
-	nitro: {
-		ignore: ['**/*.test.ts']
-	}
+	vite: { plugins: [tailwindcss()] },
+	components: [{ path: '~/components', pathPrefix: false }],
+	modules: ['@pinia/nuxt', '@vueuse/nuxt', '@nuxt/ui', '@nuxt/icon', '@nuxt/test-utils/module'],
+	nitro: { ignore: ['**/*.test.ts'] }
 });
