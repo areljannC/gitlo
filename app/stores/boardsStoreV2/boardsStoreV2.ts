f68d1837c51625b7acc0011b027b3a9acb85868f
AJ Clemente <areljann.c@gmail.com>
2025-04-18 10:18:39 -0400

    Create a store for boards
    Why `...V2`? Because I made this app's data management way too complicated in the first go. This version is KISS-ified.

---
+++ app/stores/boardsStoreV2/boardsStoreV2.ts
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia';
+import { useColumnsStore } from '../columnsStoreV2';
+import { generateHash, getTimestamp } from '~/shared/utils';
+import type { Board, NewBoard } from '~/types';
+
+export const useBoardsStore = defineStore('boardsV2', {
+	persist: { pick: ['boardIds', 'boardMap'] },
+	state: () => ({
+		boardIds: [] as string[],
+		boardMap: {} as Record<string, Board>
+	}),
+	getters: {
+		isValidBoardId:
+			state =>
+			(boardId: string): boolean =>
+				boardId in state.boardMap,
+		boardsCount: state => state.boardIds.length,
+		getBoardById:
+			state =>
+			(boardId: string): Board | undefined =>
+				state.boardMap[boardId]
+	},
+	actions: {
+		createBoard(newBoard: NewBoard): void {
+			const boardId = generateHash();
+			const currentTimestamp = getTimestamp();
+			const columnsStore = useColumnsStore();
+			this.boardIds.push(boardId);
+			this.boardMap[boardId] = {
+				id: boardId,
+				name: newBoard.name,
+				description: newBoard.description,
+				tags: newBoard.tags,
+				columnIds: Array.from({ length: newBoard.columns }, () => columnsStore.createColumn(boardId)),
+				archived: false,
+				createdAt: currentTimestamp,
+				updatedAt: currentTimestamp
+			};
+		}
+	}
+});
