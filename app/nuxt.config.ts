cee25ec9b2fb7541e4de524dbc03561aefdf6440
AJ Clemente <areljann.c@gmail.com>
2025-04-18 10:13:09 -0400

    Update configs

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -3,10 +3,15 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineNuxtConfig({
 	telemetry: false,
 	compatibilityDate: '2024-11-01',
-	devtools: { enabled: false },
+	devtools: { enabled: true },
 	css: ['~/assets/css/main.css'],
 	vite: { plugins: [tailwindcss()] },
 	components: [{ path: '~/components', pathPrefix: false }],
-	modules: ['@pinia/nuxt', '@vueuse/nuxt', '@nuxt/ui', '@nuxt/icon', '@nuxt/test-utils/module'],
-	nitro: { ignore: ['**/*.test.ts'] }
+	modules: ['@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt', '@vueuse/nuxt', '@nuxt/ui', '@nuxt/icon', '@nuxt/test-utils/module'],
+	nitro: { ignore: ['**/*.test.ts'] },
+	app: {
+		head: {
+			meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1.0' }]
+		}
+	}
 });
