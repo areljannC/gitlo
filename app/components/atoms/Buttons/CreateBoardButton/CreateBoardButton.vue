79d3c8183364531c969ef82727da86358d04f08b
AJ Clemente <areljann.c@gmail.com>
2025-04-18 10:23:40 -0400

    Create a reuseable menu component

---
+++ app/components/atoms/Buttons/CreateBoardButton/CreateBoardButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const emit = defineEmits(['click']);
+const handleClick = () => {
+	emit('click');
+};
+
+const buttonClass = 'text-lg';
+const hoverEffectClass = 'hover:shadow-md hover:-translate-y-0.25 transition-transform duration-50 ease-in-out';
+</script>
+
+<template>
+	<UButton label="Create a board" color="secondary" size="md" :class="[buttonClass, hoverEffectClass]"
+		trailing-icon="heroicons:plus-solid" @click="handleClick" />
+</template>
---
+++ app/components/atoms/Buttons/SaveChangesButton/SaveChangesButton.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useBoardsStore } from '~/stores';
+import { sleep } from '~/shared/utils';
+
+const boardsStore = useBoardsStore();
+const boardsCount = boardsStore.boardsCount;
+
+const isSavingChanges = ref(false);
+const handleSaveChanges = async () => {
+	isSavingChanges.value = true;
+	try {
+		await sleep(2000);
+		//boardsStore.syncUnsyncedBoards(); // Uncomment if needed for actual logic
+	} catch (error) {
+		console.error('Failed to save changes:', error);
+	} finally {
+		isSavingChanges.value = false;
+	}
+};
+
+const buttonClass = 'text-lg';
+const hoverEffectClass = 'hover:shadow-md hover:-translate-y-0.25 transition-transform duration-50 ease-in-out';
+</script>
+
+<template>
+	<UButton v-if="boardsCount > 0" label="TODO: Save changes" :loading="isSavingChanges" trailing :color="isSavingChanges ? 'neutral' : 'primary'"
+		size="md" :class="[buttonClass, hoverEffectClass]" trailing-icon="heroicons:document-arrow-down-solid"
+		@click="handleSaveChanges" :disabled="isSavingChanges" />
+</template>
