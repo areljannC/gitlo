13c41f703f51c7b5251e453389518713b6a71379
AJ Clemente <areljann.c@gmail.com>
2025-04-18 10:20:32 -0400

    Create a button to toggle between themes

---
+++ app/components/atoms/Buttons/ToggleThemeButton/ToggleThemeButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useColorMode } from '#imports';
+
+const colorMode = useColorMode();
+const icon = computed(() => colorMode.value === 'light' ? 'heroicons:moon-solid' : 'heroicons:sun-solid');
+const toggleTheme = () => {
+	colorMode.value = colorMode.value === 'light' ? 'dark' : 'light';
+}
+
+const buttonClass = 'text-lg';
+const hoverEffectClass = 'hover:shadow-md hover:-translate-y-0.25 transition-transform duration-50 ease-in-out';
+</script>
+
+<template>
+	<UButton label="Toggle theme" color="secondary" size="md" :class="[buttonClass, hoverEffectClass]" :trailing-icon="icon"
+		@click="toggleTheme" />
+</template>
