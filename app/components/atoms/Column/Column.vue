21e0727df4d6120839ae4c4b45b1d4d10c60dea4
AJ Clemente <areljann.c@gmail.com>
2025-04-21 14:04:04 -0400

    Create components with a functional drag-drop

---
+++ app/components/atoms/Column/Column.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { computed, useTemplateRef, ref } from 'vue';
+import { onClickOutside } from '@vueuse/core'
+import { useColumnsStore } from '~/stores';
+
+const props = defineProps({
+	columnId: {
+		type: String,
+		required: true
+	},
+});
+
+const columnsStore = useColumnsStore();
+const column = computed(() => columnsStore.columnMap[props.columnId]);
+
+const editColumnNameInputRef = useTemplateRef<HTMLInputElement>('editColumnNameInputRef');
+const columnNameInput = ref(column.value.name);
+const isEditingColumnName = ref(false);
+
+const handleStartEditingColumnName = () => {
+	columnNameInput.value = column.value.name;
+	isEditingColumnName.value = true;
+}
+
+// TODO: use `valibot` to validate the name
+const handleStopEditingColumnName = () => {
+	columnsStore.columnMap[props.columnId].name = columnNameInput.value;
+	isEditingColumnName.value = false;
+}
+
+const handleTabKey = (event: KeyboardEvent) => {
+	if (event.key === 'Tab') {
+		handleStopEditingColumnName();
+	}
+}
+
+const handleEnterKey = (event: KeyboardEvent) => {
+	if (event.key === 'Enter') {
+		event.preventDefault();
+		handleStopEditingColumnName();
+	}
+}
+
+onClickOutside(editColumnNameInputRef, () => {
+	if (!isEditingColumnName.value) return;
+	handleStopEditingColumnName();
+})
+
+const baseClass = 'rounded-md flex flex-col flex-shrink-0 overflow-y-auto gap-2 p-2';
+const dimensionClass = 'w-full md:w-68 h-fit min-h-13 max-h-full ';
+const lightThemeClass = 'bg-gray-300'
+const darkThemeClass = 'dark:bg-gray-800';
+</script>
+
+<template>
+	<div :class="[baseClass, dimensionClass, lightThemeClass, darkThemeClass]">
+		<div class="w-full flex justify-between items-center gap-2">
+			<UInput ref="editColumnNameInputRef" v-model="columnNameInput" type="text" placeholder="Enter column name..."
+				color="secondary" :highlight="isEditingColumnName" class='w-full font-bold' size="lg"
+				:variant="isEditingColumnName ? 'soft' : 'ghost'" @click="handleStartEditingColumnName"
+				@keydown="handleTabKey" @keyup="handleEnterKey" />
+			<UIcon name="heroicons:arrows-up-down-solid"
+				class="size-5 draggable-column cursor-move ml-1 mr-3 md:hidden" />
+			<UIcon name="heroicons:arrows-right-left-solid"
+				class="size-5 draggable-column cursor-move ml-1 mr-3 hidden md:block" />
+		</div>
+		<Cards :columnId="column.id" :cardIds="column.cardIds" />
+	</div>
+</template>
---
+++ app/components/molecules/Cards/Cards.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import draggable from 'vuedraggable';
+
+defineProps({
+	columnId: {
+		type: String,
+		required: true,
+	},
+	cardIds: {
+		type: Array as () => string[],
+		required: true,
+		default: []
+	}
+})
+</script>
+
+<template>
+	<div class="w-full h-full flex flex-col gap-4 mb-2">
+		<draggable :list="cardIds" :item-key="(cardId: string) => cardId" handle=".draggable-card" group="cards"
+			class="flex flex-col gap-4">
+			<template #item="{ element }">
+				<Card :cardId="element" />
+			</template>
+		</draggable>
+		<CreateCard :columnId="columnId" />
+	</div>
+</template>
