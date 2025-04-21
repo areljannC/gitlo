5bb8d607733922603c324b5aebdf834a4adaa757
AJ Clemente <areljann.c@gmail.com>
2025-04-21 19:12:21 -0400

    Add `group` props to columns

--- app/components/molecules/Columns/Columns.vue
+++ app/components/molecules/Columns/Columns.vue
@@ -4,7 +4,7 @@ import draggable from 'vuedraggable';
 defineProps({
 	boardId: {
 		type: String,
-		required: true,	
+		required: true,
 	},
 	columnIds: {
 		type: Array as () => string[],
@@ -12,26 +12,12 @@ defineProps({
 		default: []
 	},
 });
-
-const nonDraggableColumnId = 'sdfsdf';
-
-//function onMove(event: { relatedContext: any; draggedContext: any }) {
-//	const draggedId = event.draggedContext.element;
-//	const relatedId = event.relatedContext.element;
-
-//	// Prevent dragging if the dragged or related item is the non-draggable column
-//	if (draggedId === nonDraggableColumnId || relatedId === nonDraggableColumnId) {
-//		return false;
-//	}
-//	return true;
-//}
-
-
 </script>
 
 <template>
 	<div class="w-full h-full flex flex-col md:flex-row gap-4 overflow-x-auto px-4">
-		<draggable :list="columnIds" :item-key="(columnId: string) => columnId" handle=".draggable-column" class="flex flex-col md:flex-row gap-4">
+		<draggable :list="columnIds" :item-key="(columnId: string) => columnId" handle=".draggable-column"
+			group="columns" class="flex flex-col md:flex-row gap-4">
 			<template #item="{ element }">
 				<Column :columnId="element" />
 			</template>
