0d0ab8f6ac7134e99a681a47bcdce4614cf51396
AJ Clemente <areljann.c@gmail.com>
2025-04-21 19:12:01 -0400

    Turn overflowing text to ellipsis

--- app/app.config.ts
+++ app/app.config.ts
@@ -4,6 +4,11 @@ export default defineAppConfig({
 		cssLayer: 'base'
 	},
 	ui: {
+		input: {
+			slots: {
+				base: 'w-full rounded-md border-0 placeholder:text-dimmed focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 transition-colors text-ellipsis'
+			}
+		},
 		modal: {
 			slots: {
 				header: 'flex justify-center items-center gap-1.5 p-4 sm:px-6 min-h-16',
