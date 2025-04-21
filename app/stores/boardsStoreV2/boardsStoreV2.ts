ebc857bad5d81f1cebc5185ecdba30ac99fb9644
AJ Clemente <areljann.c@gmail.com>
2025-04-21 14:02:22 -0400

    Update boards store

--- app/stores/boardsStoreV2/boardsStoreV2.ts
+++ app/stores/boardsStoreV2/boardsStoreV2.ts
@@ -3,7 +3,7 @@ import { useColumnsStore } from '../columnsStoreV2';
 import { generateHash, getTimestamp } from '~/shared/utils';
 import type { Board, NewBoard } from '~/types';
 
-export const useBoardsStore = defineStore('boardsV2', {
+export const useBoardsStore = defineStore('Boards', {
 	persist: { pick: ['boardIds', 'boardMap'] },
 	state: () => ({
 		boardIds: [] as string[],
@@ -21,7 +21,7 @@ export const useBoardsStore = defineStore('boardsV2', {
 				state.boardMap[boardId]
 	},
 	actions: {
-		createBoard(newBoard: NewBoard): void {
+		createBoard(newBoard: NewBoard): string {
 			const boardId = generateHash();
 			const currentTimestamp = getTimestamp();
 			const columnsStore = useColumnsStore();
@@ -36,6 +36,7 @@ export const useBoardsStore = defineStore('boardsV2', {
 				createdAt: currentTimestamp,
 				updatedAt: currentTimestamp
 			};
+			return boardId
 		}
 	}
 });
