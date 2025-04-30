f7234c90c073774df77e4d6ec9660d2c0de2c643
AJ Clemente <areljann.c@gmail.com>
2025-04-29 20:59:33 -0400

    Delete modal header customization

--- app/app.config.ts
+++ app/app.config.ts
@@ -11,7 +11,6 @@ export default defineAppConfig({
 		},
 		modal: {
 			slots: {
-				header: 'flex justify-center items-center gap-1.5 p-4 sm:px-6 min-h-16',
 				body: 'flex-1 overflow-y-auto p-4 sm:p-6',
 				footer: 'flex items-center gap-2 p-4 sm:px-6 justify-end'
 			}
