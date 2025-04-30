f260f6e8e51f9f22ae0d39fa645df2c94a018f0f
AJ Clemente <areljann.c@gmail.com>
2025-04-29 21:40:37 -0400

    Mock system time to fix intermittent unit test failures

--- app/shared/utils/getTimestamp/getTimestamp.test.ts
+++ app/shared/utils/getTimestamp/getTimestamp.test.ts
@@ -1,11 +1,23 @@
-import { describe, it, expect } from 'vitest';
+import { describe, beforeAll, afterAll, vi, it, expect } from 'vitest';
 import { getTimestamp } from './getTimestamp';
 
 describe('getTimestamp', () => {
+	const fixedDate = new Date('2025-01-01T00:00:00.000Z');
+
+	beforeAll(() => {
+        vi.useFakeTimers();
+        vi.setSystemTime(fixedDate);
+    });
+
+    afterAll(() => {
+        vi.useRealTimers();
+    });
+
 	it('returns an ISO timestamp of type `string`', () => {
 		const timestamp = getTimestamp();
 		expect(timestamp).toBeTypeOf('string');
 	});
+
 	it('returns a valid ISO timestamp', () => {
 		const timestamp = getTimestamp();
 		const currentTimestamp = new Date().toISOString();
