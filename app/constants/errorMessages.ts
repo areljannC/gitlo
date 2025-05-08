7d7ce418ba54e1a64fbdfb8f1880c6360c8adb72
AJ Clemente <areljann.c@gmail.com>
2025-05-07 21:54:48 -0400

    Create store error message constants

---
+++ app/constants/errorMessages.ts
@@ -0,0 +1,32 @@
+export const BOARD_ERROR = Object.freeze({
+	ID_ALREADY_EXISTS: 'Board ID already exists.',
+	ID_INVALID: 'Invalid board ID.',
+	CREATE_BOARD: 'Error creating a new board.',
+	UPDATE_BOARD: 'Error updating board.',
+	ARCHIVE_BOARD: 'Error archiving board.',
+	UNARCHIVE_BOARD: 'Error unarchiving board.',
+	DELETE_BOARD: 'Error deleting board.'
+});
+
+export const COLUMN_ERROR = Object.freeze({
+	ID_ALREADY_EXISTS: 'Column ID already exists.',
+	ID_INVALID: 'Invalid column ID.',
+	CREATE_COLUMN: 'Error creating a new column.',
+	UPDATE_COLUMN: 'Error updating column.',
+	ARCHIVE_COLUMN: 'Error archiving column.',
+	UNARCHIVE_COLUMN: 'Error unarchiving column.',
+	DELETE_COLUMN: 'Error deleting column.'
+});
+
+export const CARD_ERROR = Object.freeze({
+	ID_ALREADY_EXISTS: 'Card ID already exists.',
+	ID_INVALID: 'Invalid card ID.',
+	CREATE_CARD: 'Error creating a new card.',
+	UPDATE_CARD: 'Error updating card.',
+	ARCHIVE_CARD: 'Error archiving card.',
+	UNARCHIVE_CARD: 'Error unarchiving card.',
+	DELETE_CARD: 'Error deleting card.',
+	MOVE_CARD: 'Error moving card to column.',
+	EXPAND_CARD: 'Error expanding card.',
+	COLLAPSE_CARD: 'Error collapsing card.'
+});
--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,1 +1,2 @@
 export * from './apiMessages';
+export * from './errorMessages';
