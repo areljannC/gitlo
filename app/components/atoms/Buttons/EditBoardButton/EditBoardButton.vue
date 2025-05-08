3a710b8125dd90fa8495e3c39e33508b410ed0f7
AJ Clemente <areljann.c@gmail.com>
2025-05-08 12:34:35 -0400

    Create unit tests for edit board button atom component

---
+++ app/components/atoms/Buttons/EditBoardButton/EditBoardButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const emit = defineEmits(['edit']);
+const handleEdit = () => {
+	emit('edit');
+};
+
+const buttonClass = 'text-lg';
+const hoverEffectClass = 'hover:shadow-md hover:-translate-y-0.25 transition-transform duration-50 ease-in-out';
+</script>
+
+<template>
+	<UButton label="Edit board" color="secondary" size="md" :class="[buttonClass, hoverEffectClass]"
+		trailing-icon="heroicons:pencil-solid" @click="handleEdit" />
+</template>
