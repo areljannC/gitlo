ff556b0e032d697e293671befacce4674340ebad
AJ Clemente <areljann.c@gmail.com>
2025-05-09 14:01:40 -0400

    Fix various issues to card atom component
    - card name not updating when updated
    - save card name changes when pressing Tab key
    -  use text area instead of text input to update card name

--- app/components/atoms/Card/Card.vue
+++ app/components/atoms/Card/Card.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import { computed, useTemplateRef, ref } from 'vue';
+import { computed, useTemplateRef, ref, watch } from 'vue';
 import { onClickOutside } from '@vueuse/core'
 import { useCardsStore } from '~/stores';
-import { updateCard } from '~/services';
 
 const props = defineProps({
 	cardId: {
@@ -18,31 +17,34 @@ const editCardNameInputRef = useTemplateRef<HTMLInputElement>('editCardNameInput
 const cardNameInput = ref(card.value.name);
 const isEditingCardName = ref(false);
 
+watch(card, updatedCard => {
+	cardNameInput.value = updatedCard.name
+});
+
 const handleStartEditingCardName = () => {
 	cardNameInput.value = card.value.name;
 	isEditingCardName.value = true;
 }
 
 // TODO: use `valibot` to validate the name
+// TODO: wrap this in a try-catch block
 const handleStopEditingCardName = () => {
 	const cardName = cardNameInput.value.trim();
 	if (cardName !== '' && cardName.length > 0) {
-		updateCard(props.cardId, { name: cardName });
+		cardsStore.updateCard(props.cardId, { name: cardName });
 		isEditingCardName.value = false;
 	} else {
 		console.warn('Invalid card name.');
 	}
 }
 
-const handleTabKey = (event: KeyboardEvent) => {
-	if (event.key === 'Tab') {
-		handleStopEditingCardName();
-	}
-}
-
-const handleEnterKey = (event: KeyboardEvent) => {
+// TODO: make this better
+const handleKeyDown = (event: KeyboardEvent) => {
 	if (event.key === 'Enter') {
 		event.preventDefault();
+		handleStopEditingCardName();
+	}
+	if (event.key === 'Tab') {
 		handleStopEditingCardName();
 	}
 }
@@ -52,24 +54,26 @@ onClickOutside(editCardNameInputRef, () => {
 	handleStopEditingCardName();
 })
 
+const handleExpandCard = () => {
+	cardsStore.expandCard(props.cardId);
+}
+
 const baseClass = 'rounded-md flex-shrink-0 p-2';
-const dimensionClass = 'w-full h-fit min-h-13'
+const dimensionClass = 'w-full h-fit min-h-13 max'
 const lightThemeClass = 'bg-gray-100'
 const darkThemeClass = 'dark:bg-gray-600';
-
-const NOOP = () => { };
 </script>
 
 <template>
 	<div :class="[baseClass, dimensionClass, lightThemeClass, darkThemeClass]">
-		<div class="w-full flex justify-between items-center gap-1 pr-2">
-			<UInput ref="editCardNameInputRef" v-model="cardNameInput" type="text" placeholder="Enter card name..."
-				color="secondary" :highlight="isEditingCardName" class='w-full font-bold' size="lg"
-				:variant="false ? 'soft' : 'ghost'" @click="handleStartEditingCardName" @keydown="handleTabKey"
-				@keyup="handleEnterKey" />
-			<div class="flex gap-1 items-center">
+		<div class="w-full flex justify-between items-start gap-1 pr-2">
+			<UTextarea ref="editCardNameInputRef" v-model="cardNameInput" placeholder="Enter card name..."
+				color="secondary" :highlight="isEditingCardName" class="w-full font-bold" size="lg"
+				:variant="isEditingCardName ? 'soft' : 'ghost'" :rows="1" :maxrows="0" autoresize :autoresizeDelay="0"
+				@click="handleStartEditingCardName" @keydown="handleKeyDown" />
+			<div class="w-fit h-fit flex gap-1 items-center">
 				<UButton icon="heroicons:arrows-pointing-out-20-solid" class="size-5 w-fit h-fit cursor-pointer"
-					variant="ghost" color="neutral" />
+					variant="ghost" color="neutral" @click="handleExpandCard" />
 				<UIcon name="heroicons:arrows-up-down-solid"
 					class="size-5 draggable-card md:hidden hover:cursor-grab active:cursor-grabbing" />
 				<UIcon name="icon-park-outline:direction-adjustment-two"
