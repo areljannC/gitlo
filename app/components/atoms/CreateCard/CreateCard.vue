51f38d79af34f22a6d7641a003021c80bfa3103b
AJ Clemente <areljann.c@gmail.com>
2025-05-11 23:58:27 -0400

    Fix various issues to create card atom component

--- app/components/atoms/CreateCard/CreateCard.vue
+++ app/components/atoms/CreateCard/CreateCard.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { useTemplateRef, ref } from 'vue';
-import { onClickOutside } from '@vueuse/core'
-import { createCardWithName } from '~/services';
+import { useCardsStore } from '~/stores';
 
 const props = defineProps({
 	columnId: {
@@ -10,39 +9,39 @@ const props = defineProps({
 	}
 });
 
+const cardsStore = useCardsStore();
 const createCardInputRef = useTemplateRef<HTMLInputElement>('createCardInputRef');
 const cardNameInput = ref('');
 const isEditingCardName = ref(false);
 
 const handleStartEditingCardName = () => {
-	cardNameInput.value = '';
 	isEditingCardName.value = true;
 }
 
 // TODO: use`valibot` to validate the name
 const handleStopEditingCardName = () => {
-	const cardName = cardNameInput.value.trim();
-	if (cardName !== '' && cardName.length > 0) {
-		createCardWithName(props.columnId, cardName)
+	try {
+		const cardName = cardNameInput.value.trim();
+		if (cardName.length === 0) {
+			isEditingCardName.value = false;
+			return;
+		}
+		cardsStore.createCard(props.columnId, { name: cardName });
 		cardNameInput.value = '';
 		isEditingCardName.value = false;
-	} else {
-		console.warn('Invalid card name.');
+	} catch (error) {
+		console.error(error);
 	}
 }
 
-const handleKeyUp = (event: KeyboardEvent) => {
-	event.preventDefault();
-	if (event.key !== 'Enter') return;
-	handleStopEditingCardName();
+const handleKeyDown = (event: KeyboardEvent) => {
+	if (event.key === 'Enter') {
+		event.preventDefault();
+		handleStopEditingCardName();
+	}
 }
 
-onClickOutside(createCardInputRef, () => {
-	if (!isEditingCardName.value) return;
-	handleStopEditingCardName();
-})
-
-const baseClass = 'rounded-md flex-shrink-0 p-2 mt-4 opacity-50 hover:opacity-100 transition-opacity duration-200 ease-in-out';
+const baseClass = 'rounded-md flex-shrink-0 p-2 opacity-50 hover:opacity-100 transition-opacity duration-200 ease-in-out';
 const dimensionClass = 'w-full h-fit min-h-13'
 const lightThemeClass = 'bg-gray-100'
 const darkThemeClass = 'dark:bg-gray-600';
@@ -50,9 +49,9 @@ const darkThemeClass = 'dark:bg-gray-600';
 
 <template>
 	<div :class="[baseClass, dimensionClass, lightThemeClass, darkThemeClass]">
-		<UInput ref="createCardInputRef" v-model="cardNameInput" type="text" placeholder="Add a card..."
-			color="secondary" icon="heroicons:plus-solid" :highlight="isEditingCardName" class='w-full font-bold'
-			size="lg" :variant="isEditingCardName ? 'soft' : 'ghost'" @click="handleStartEditingCardName"
-			@keyup="handleKeyUp" />
+		<UTextarea ref="createCardInputRef" v-model="cardNameInput" placeholder="Add a card..." color="secondary"
+			icon="heroicons:plus-solid" :highlight="isEditingCardName" class="w-full font-bold" size="lg"
+			:variant="isEditingCardName ? 'soft' : 'ghost'" :rows="1" :maxrows="0" autoresize :autoresizeDelay="0"
+			@focus="handleStartEditingCardName" @blur="handleStopEditingCardName" @keydown="handleKeyDown" />
 	</div>
 </template>
