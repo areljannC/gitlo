5c7a26ac31d296aa6a2e6b058f9ae796e22ce0c4
AJ Clemente <areljann.c@gmail.com>
2025-05-13 14:05:46 -0400

    Hide card body if board has no description or tags

--- app/components/molecules/BoardPreview/BoardPreview.vue
+++ app/components/molecules/BoardPreview/BoardPreview.vue
@@ -25,16 +25,25 @@ const descriptionClass = 'text-sm md:text-md lg:text-lg break-all';
 </script>
 
 <template>
-	<UCard :class="[dimensionsClass, hoverEffectClass]">
+	<UCard v-if="description !== '' || tags.length > 0" :class="[dimensionsClass, hoverEffectClass]">
 		<template #header>
 			<h1 :class="nameClass">{{ name }}</h1>
 		</template>
 		<div class="flex flex-col gap-4">
-			<p v-if="description" :class="descriptionClass">{{ description }}</p>
+			<p v-if="description !== ''" :class="descriptionClass">{{ description }}</p>
 			<div v-if="tags.length > 0" class="flex flex-wrap gap-2">
 				<Tag v-for="tag in [...tags]" :key="tag" :label="tag" />
 			</div>
 		</div>
+		<template #footer>
+			<UButton label="View" color="secondary" variant="ghost" class="w-full flex justify-center"
+				@click="handleViewBoard" />
+		</template>
+	</UCard>
+	<UCard v-else :class="[dimensionsClass, hoverEffectClass]">
+		<template #header>
+			<h1 :class="nameClass">{{ name }}</h1>
+		</template>
 		<template #footer>
 			<UButton label="View" color="secondary" variant="ghost" class="w-full flex justify-center"
 				@click="handleViewBoard" />
