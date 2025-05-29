71bf924e4cdb15732e8882f82a298ff709f506c4
AJ Clemente <areljann.c@gmail.com>
2025-05-28 23:45:19 -0400

    Create a reusable schema for a column

---
+++ app/schemas/columnSchema/columnSchema.ts
@@ -0,0 +1,15 @@
+import * as v from 'valibot';
+
+// TODO: localization
+// TODO: unit test
+export const ERROR = Object.freeze({
+	ID: {
+		NON_EMPTY: 'Column ID is required.'
+	},
+	NAME: Object.freeze({
+		MAX_LENGTH: 'Board name must be shorter than 32 characters.'
+	})
+});
+
+export const getIdValidator = () => v.pipe(v.string(), v.trim(), v.nonEmpty(ERROR.ID.NON_EMPTY));
+export const getNameValidator = () => v.pipe(v.string(), v.trim(), v.maxLength(32, ERROR.NAME.MAX_LENGTH));
