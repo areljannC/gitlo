188701fdb54cb488917b3b4d69c51caa42a779cb
AJ Clemente <areljann.c@gmail.com>
2025-07-30 12:04:35 -0400

    Remove `/api` directory
    I decided that this app should just handle everything on the client-side. This makes the app less complex as we no longer have to worry about API calls.

--- app/server/api/data/load.get.ts
+++
@@ -1,7 +0,0 @@
-import { defineEventHandler } from 'h3';
-import { getTimestamp } from '~/shared/utils';
-
-export default defineEventHandler(event => {
-	setResponseStatus(event, 200);
-	return { message: 'load', timestamp: getTimestamp() };
-});
