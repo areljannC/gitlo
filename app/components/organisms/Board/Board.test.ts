11c77a27d3d1399813e6e45e319abcb6937216d6
AJ Clemente <areljann.c@gmail.com>
2025-07-31 10:56:02 -0400

    Create placeholder component unit test files
    NOTE: I'm just too lazy to write any unit tests right now. I'll come back to these unit tests after v0.1.0 is complete.

--- app/components/organisms/Board/Board.test.ts
+++ app/components/organisms/Board/Board.test.ts
@@ -1,10 +1,10 @@
 import { describe, it, expect } from 'vitest';
 import { mountSuspended, mockNuxtImport } from '@nuxt/test-utils/runtime';
-import LoadBoardButton from './LoadBoardButton.vue';
+import Board from './Board.vue';
 
 mockNuxtImport('useColorMode', () => {
 	const colorMode = ref('light');
 	return () => colorMode
 });
 
-describe.skip('LoadBoardButton', () => {});
+describe.skip('Board', () => {});
