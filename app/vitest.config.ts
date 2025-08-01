ad4f9de616ed71a332fc03b7f825048f4866f7e3
AJ Clemente <areljann.c@gmail.com>
2025-08-01 16:07:02 -0400

    Enable `100%` code coverage

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -8,13 +8,13 @@ export default defineVitestConfig({
 			provider: 'v8',
 			reporter: ['text', 'html', 'json'],
 			reportsDirectory: './coverage',
-			//thresholds: {
-			//	lines: 100,
-			//	functions: 100,
-			//	branches: 100,
-			//	statements: 100,
-			//	perFile: true
-			//},
+			thresholds: {
+				lines: 100,
+				functions: 100,
+				branches: 100,
+				statements: 100,
+				perFile: true
+			},
 			all: true,
 			clean: true,
 			cleanOnRerun: true,
