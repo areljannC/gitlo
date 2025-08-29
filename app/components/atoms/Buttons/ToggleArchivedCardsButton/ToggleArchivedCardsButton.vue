08f669e854f933bffa6a5cf05e781cc5bbfdad09
AJ Clemente <areljann.c@gmail.com>
2025-08-29 13:33:25 -0400

    Create `ToggleArchivedCardsButton` atom component

---
+++ app/components/atoms/Buttons/ToggleArchivedCardsButton/ToggleArchivedCardsButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useSettingsStore } from '~/stores/settingsStore/settingsStore';
+
+const settingsStore = useSettingsStore();
+const showArchivedCards = computed(() => settingsStore.showArchivedCards);
+const toggleArchivedCards = () => {
+	settingsStore.setShowArchivedCards(!showArchivedCards.value);
+}
+
+const buttonClass = 'text-lg';
+const hoverEffectClass = 'hover:shadow-md hover:-translate-y-0.25 transition-transform duration-50 ease-in-out';
+</script>
+
+<template>
+	<UButton :label="showArchivedCards ? 'Hide archived cards' : 'Show archived cards'" color="secondary" size="md"
+		:class="[buttonClass, hoverEffectClass]"
+		:trailing-icon="showArchivedCards ? 'heroicons:eye-slash' : 'heroicons:eye'" @click="toggleArchivedCards" />
+</template>
