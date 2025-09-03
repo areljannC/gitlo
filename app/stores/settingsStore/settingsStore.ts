400438731e82e42c715d822528cf12a0341b86f3
AJ Clemente <areljann.c@gmail.com>
2025-09-03 11:27:09 -0400

    Add `showArchivedColumns` setting in settings store

--- app/stores/settingsStore/settingsStore.ts
+++ app/stores/settingsStore/settingsStore.ts
@@ -2,12 +2,16 @@ import { defineStore } from 'pinia';
 
 export const useSettingsStore = defineStore('settings', {
 	persist: {
-		pick: ['showArchivedCards']
+		pick: ['showArchivedColumns', 'showArchivedCards']
 	},
 	state: () => ({
+		showArchivedColumns: false as boolean,
 		showArchivedCards: false as boolean
 	}),
 	actions: {
+		setShowArchivedColumns(value: boolean) {
+			this.showArchivedColumns = value;
+		},
 		setShowArchivedCards(value: boolean) {
 			this.showArchivedCards = value;
 		}
