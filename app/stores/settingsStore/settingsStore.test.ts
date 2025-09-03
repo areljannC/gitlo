4925f1a0db51968a8aea8f8e825c7c0be2398d7f
AJ Clemente <areljann.c@gmail.com>
2025-09-03 11:27:09 -0400

    Add `showArchivedColumns` setting in settings store

--- app/stores/settingsStore/settingsStore.test.ts
+++ app/stores/settingsStore/settingsStore.test.ts
@@ -13,11 +13,28 @@ describe('Settings Store', () => {
 	describe('state', () => {
 		it('should have the correct initial state', () => {
 			const settingsStore = useSettingsStore();
+			expect(settingsStore.showArchivedColumns).toBe(false);
 			expect(settingsStore.showArchivedCards).toBe(false);
 		});
 	});
 
 	describe('actions', () => {
+		describe('setShowArchivedColumns', () => {
+			it('should set `showArchivedColumns` to `true`', () => {
+				const settingsStore = useSettingsStore();
+				settingsStore.setShowArchivedColumns(true);
+				expect(settingsStore.showArchivedColumns).toBe(true);
+			});
+
+			it('should set `showArchivedColumns` to `false`', () => {
+				const settingsStore = useSettingsStore();
+				settingsStore.setShowArchivedColumns(true);
+				expect(settingsStore.showArchivedColumns).toBe(true);
+				settingsStore.setShowArchivedColumns(false);
+				expect(settingsStore.showArchivedColumns).toBe(false);
+			});
+		});
+
 		describe('setShowArchivedCards', () => {
 			it('should set `showArchivedCards` to `true`', () => {
 				const settingsStore = useSettingsStore();
