d18534db316af4cbb1187a48bfb1fc95b73a43ab
AJ Clemente <areljann.c@gmail.com>
2025-09-09 13:21:24 -0400

    Hide archived boards in `Boards` organism component

--- app/components/organisms/Boards/Boards.vue
+++ app/components/organisms/Boards/Boards.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { useBoardsStore } from '~/stores';
+import { computed, ref, watch } from 'vue';
+import { useSettingsStore, useBoardsStore } from '~/stores';
 import type { Board } from '~/types';
 
+const settingsStore = useSettingsStore();
 const boardsStore = useBoardsStore();
-const boardIds = computed(() => boardsStore.getBoardIds());
-//const archivedBoardIds = computed(() => boardsStore.getArchivedBoardIds());
 const boardMap = computed(() => boardsStore.boardMap);
+const boardIds = computed(() => boardsStore.getBoardIds());
+const archivedBoardIds = computed(() => boardsStore.getArchivedBoardIds());
+const visibleBoardIds = computed(() => settingsStore.showArchivedBoards
+	? [...boardIds.value, ...archivedBoardIds.value]
+	: boardIds.value
+);
 
 const isCreateBoardModalOpen = ref(false);
 
@@ -33,7 +38,7 @@ const handleViewBoard = (boardId: string) => {
 
 <template>
 	<!-- no boards -->
-	<UContainer v-if="boardIds.length === 0"
+	<UContainer v-if="visibleBoardIds.length === 0"
 		class="w-screen h-screen flex flex-col justify-center items-center text-center gap-4 p-4">
 		<h1 class="font-bold text-3xl">Boards</h1>
 		<p class="text-lg">You have <span class="font-bold">no boards</span>.</p>
@@ -44,8 +49,9 @@ const handleViewBoard = (boardId: string) => {
 	<UContainer v-else class="w-screen flex flex-col items-center gap-4 p-4">
 		<h1 class="font-bold text-3xl">Boards</h1>
 		<div class="w-full flex flex-wrap gap-4 justify-start">
-			<BoardPreview v-for="boardId of boardIds" :key="boardId" :name="boardMap[boardId].name"
-				:description="boardMap[boardId].description" :tags="boardMap[boardId].tags" @view="handleViewBoard(boardId)" />
+			<BoardPreview v-for="boardId of visibleBoardIds" :key="boardId" :name="boardMap[boardId].name"
+				:description="boardMap[boardId].description" :tags="boardMap[boardId].tags"
+				@view="handleViewBoard(boardId)" />
 		</div>
 	</UContainer>
 
