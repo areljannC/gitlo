949185eae3ed00471099f2708aa34095561cd4f5
AJ Clemente <areljann.c@gmail.com>
2025-09-10 15:05:48 -0400

    Create `CancelButton` atom component with unit test

---
+++ app/components/atoms/Buttons/CancelButton/CancelButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const emit = defineEmits(['cancel']);
+
+const handleCancel = () => {
+	emit('cancel');
+};
+</script>
+
+<template>
+	<UButton label="Cancel" color="error" variant="soft" @click="handleCancel" />
+</template>
