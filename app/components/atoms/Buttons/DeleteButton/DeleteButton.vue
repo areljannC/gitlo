9487a51a6ffd0203726f4cffd0c2abfae639bc84
AJ Clemente <areljann.c@gmail.com>
2025-09-10 15:06:04 -0400

    Create `DeleteButton` atom component with unit test

---
+++ app/components/atoms/Buttons/DeleteButton/DeleteButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const emit = defineEmits(['delete']);
+
+const handleDelete = () => {
+	emit('delete');
+};
+</script>
+
+<template>
+	<UButton label="Delete" color="error" variant="soft" @click="handleDelete" />
+</template>
