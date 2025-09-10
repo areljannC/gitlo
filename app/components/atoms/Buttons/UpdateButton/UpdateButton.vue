5802814382fbcf6f05f41bc2eec4ccd4044738b0
AJ Clemente <areljann.c@gmail.com>
2025-09-10 15:06:20 -0400

    Create `UpdateButton` atom component with unit test

---
+++ app/components/atoms/Buttons/UpdateButton/UpdateButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const emit = defineEmits(['update']);
+
+const handleUpdate = () => {
+	emit('update');
+};
+</script>
+
+<template>
+	<UButton label="Update" color="primary" @click="handleUpdate" />
+</template>
