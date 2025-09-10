30f3c73f5efa9f4dbdc6a7bb8f0716628a5e955f
AJ Clemente <areljann.c@gmail.com>
2025-09-10 11:28:49 -0400

    Update `Boards` organism component unit test

--- app/components/organisms/Boards/Boards.vue
+++ app/components/organisms/Boards/Boards.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
+import { computed, ref } from 'vue';
 import { useSettingsStore, useBoardsStore } from '~/stores';
 import type { Board } from '~/types';
 
@@ -18,6 +18,7 @@ const isCreateBoardModalOpen = ref(false);
 const handleOpenCreateBoardModal = () => {
 	isCreateBoardModalOpen.value = true;
 };
+
 const handleCloseCreateBoardModal = () => {
 	isCreateBoardModalOpen.value = false;
 };
