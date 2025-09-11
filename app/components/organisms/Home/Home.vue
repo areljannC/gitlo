3c289a40d009c24631916ffabf3e0ed7cb409de3
AJ Clemente <areljann.c@gmail.com>
2025-09-11 12:33:48 -0400

    Configure basic page document titles

--- app/components/organisms/Home/Home.vue
+++ app/components/organisms/Home/Home.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { useHead, navigateTo } from '#app';
+
+useHead({ title: 'gitlo.app' });
+
 const handleGoToBoards = () => {
 	navigateTo('/boards');
 };
@@ -6,7 +10,7 @@ const handleGoToBoards = () => {
 
 <template>
 	<UContainer class="w-screen h-screen flex flex-col justify-center items-center space-y-4">
-		<h1 class="font-bold text-3xl">gitlo</h1>
+		<h1 class="font-bold text-3xl">gitlo.app</h1>
 		<UButton label="View Boards" color='secondary' @click="handleGoToBoards" class="text-lg" />
 	</UContainer>
 </template>
--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -12,7 +12,11 @@ export default defineNuxtConfig({
 	nitro: { ignore: ['**/*.test.ts'] },
 	app: {
 		head: {
-			meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1.0' }]
+			title: 'gitlo.app',
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1.0' }
+			]
 		}
 	}
 });
